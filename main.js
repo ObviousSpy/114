e2e1d5ce3f34b2f17a4f167fa28fc5c30e85b82b
ObviousSpy <79284823+ObviousSpy@users.noreply.github.com>
2021-12-16 17:55:29 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload() {   
+}
+
+function setup() {
+canvas = createCanvas(300, 300);
+canvas.center();
+canvas.position(500, 250)
+}
+
+function draw() {
+}
+
+function take_snapshot() {
+save('St.FairyImage.png');
+}
